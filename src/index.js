b323133c5f418c1bac2a5cd7dab2b88108f3b64e
Benjamin PERNEY <benjperney@gmail.com>
2021-03-06 13:26:20 +0100

    Added routing

--- src/index.js
+++ src/index.js
@@ -1,112 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './home';
+import About from './about';
+import Error from './error';
 
-function Square(props) {
-    return (
-      <button className="square" onClick={ props.onClick }>
-        { props.value }
-      </button>
-    );
-}
-
-class Board extends React.Component {
-
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i) }/>;
-  }
-
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.XIsNext ? `X` : `O`;
-    this.setState({
-      squares: squares,
-      XIsNext: !this.state.XIsNext
-    })
-  }
-
-  render() {
-    let status = `Next player: ${this.state.XIsNext ? 'X' : 'O'}`;
-    const winner = calculateWinner(this.state.squares);
-    if (winner) {
-      status = `Winner is ${winner}`;
-    }
-
-
-    return (
-      <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
 
 class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [{
-        squares: Array(9).fill(null),
-      }],
-      xIsNext: true,
-    };
-  }
+
   render() {
+
     return (
-      <div className="game">
-        <div className="game-board">
-          <Board />
-        </div>
-        <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
-        </div>
-      </div>
+      <main>
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/about' component={About} />
+          <Route component={Error} />
+        </Switch>
+      </main>
+
     );
   }
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
 
 // ========================================
 
 ReactDOM.render(
-  <Game />,
+  <BrowserRouter>
+    <Game />
+  </BrowserRouter>,
   document.getElementById('root')
 );
