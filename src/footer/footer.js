ca728abfa76907e8fcc8a67207605a64247ac99c
Benjamin PERNEY <benjperney@gmail.com>
2021-03-07 18:30:26 +0100

    Added social networks

--- src/footer/footer.js
+++ src/footer/footer.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import './footer.scss';
+import github from '../assets/github.svg'
+import linkedin from '../assets/linkedin.svg'
 
 export default class Footer extends Component {
   render() {
@@ -7,7 +9,13 @@ export default class Footer extends Component {
       <div>
         <footer>
           <div className="footer--wrapper">
-            <p className="footer--content">Contact me:<br/> benjperney[at]gmail.com</p>
+            <p className="footer--content">
+              Contact me:<br/> benjperney[at]gmail.com
+            </p>
+            <p className="footer--content">
+              <a href="https://github.com/Bbemol" target="_blank"><img className="icons" src={github} /></a>
+              <a href="https://www.linkedin.com/in/benjamin-perney-6324b74a/" target="_blank"><img className="icons" src={linkedin} /></a>
+            </p>
           </div>
         </footer>
       </div>
